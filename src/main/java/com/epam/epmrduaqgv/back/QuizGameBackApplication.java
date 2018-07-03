a1da706913312456ac2807c3d2e9b994e9fc1410
Oleksii_Kovetskyi <Oleksii_Kovetskyi@epam.com>
2018-07-03 22:52:23 +0300

    Initial commit

---
+++ src/main/java/com/epam/epmrduaqgv/back/QuizGameBackApplication.java
@@ -0,0 +1,12 @@
+package com.epam.epmrduaqgv.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGameBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGameBackApplication.class, args);
+	}
+}
