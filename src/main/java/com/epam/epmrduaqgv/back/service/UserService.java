896983ae3a056c119a2a0946102471249e242973
Oleksii_Kovetskyi <Oleksii_Kovetskyi@epam.com>
2018-07-09 16:57:05 +0300

    Extended UserRepository and UserService. Liquibase scripts changes.

--- src/main/java/com/epam/epmrduaqgv/back/service/UserService.java
+++ src/main/java/com/epam/epmrduaqgv/back/service/UserService.java
@@ -1,7 +1,14 @@
 package com.epam.epmrduaqgv.back.service;
 
 import com.epam.epmrduaqgv.back.entity.UserEntity;
+import com.epam.epmrduaqgv.back.form.SignUpForm;
 
 public interface UserService {
     UserEntity findByEmail(String email);
+
+    UserEntity findByNickName(String nickName);
+
+    UserEntity findByEmailOrNickName(String email, String nickName);
+
+    UserEntity registerUser(SignUpForm form);
 }
