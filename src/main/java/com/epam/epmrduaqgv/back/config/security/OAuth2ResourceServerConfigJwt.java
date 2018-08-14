21e0b61b1343a0afce86cd813edf5e794a43bfcb
Oleksii_Kovetskyi <Oleksii_Kovetskyi@epam.com>
2018-08-14 10:30:29 +0300

    Changed score for answer, enabled h2console for test profile

--- src/main/java/com/epam/epmrduaqgv/back/config/security/OAuth2ResourceServerConfigJwt.java
+++ src/main/java/com/epam/epmrduaqgv/back/config/security/OAuth2ResourceServerConfigJwt.java
@@ -1,7 +1,9 @@
 package com.epam.epmrduaqgv.back.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -9,14 +11,25 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class OAuth2ResourceServerConfigJwt extends ResourceServerConfigurerAdapter {
 
+    @Value("${spring.h2.console.enabled:false}")
+    private boolean h2consoleEnabled;
+
+    @Value("${spring.h2.console.path:}")
+    private String h2consolePath;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry urlRegistry = http.authorizeRequests()
                 .antMatchers("/health", "/register").permitAll()
                 .antMatchers("/swagger-ui.html",
                         "/swagger-resources/**",
                         "/webjars/**",
-                        "/v2/api-docs/**").permitAll()
-                .anyRequest().authenticated();
+                        "/v2/api-docs/**").permitAll();
+        if (h2consoleEnabled) {
+            urlRegistry = urlRegistry
+                    .antMatchers(h2consolePath + "/**")
+                    .permitAll();
+        }
+        urlRegistry.anyRequest().authenticated();
     }
 }
